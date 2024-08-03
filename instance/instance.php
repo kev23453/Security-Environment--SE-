d90d000ca3a97d33a7dc1f5e8a9327670d3fe1ce
kev23453 <kevingarcia22120514@gmail.com>
2024-08-03 01:42:40 -0400

    ADD: directory admin and instance
    we create 2 directory that call admin with an archive of dashboard and create other directory call instance that will save the criminal instances

---
+++ instance/instance.php
@@ -0,0 +1,11 @@
+<?php
+// aqui se registraran los casos
+
+
+$route_c = "../conexion.php";
+$route_lg = "../authentication/account.php";
+
+include '../conexion.php';
+include '../authentication/sesion.php';
+
+?>
